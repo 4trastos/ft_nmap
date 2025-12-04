a72031c8211606457375d418b44c8da6bce8f3f8
4trastos <info@4trastos.com>
2025-12-04 20:46:23 +0100

    scanning

--- src/data/scan_ports.c
+++ src/data/scan_ports.c
@@ -6,7 +6,7 @@ int    scan_port(t_thread_context *ctx, int port)
     struct sockaddr_in  dest;
     struct sockaddr_in  recv_adr;
     socklen_t           addr_len_recv;
-    unsigned char       nuffer[512];
+    unsigned char       buffer[512];
     ssize_t             sent_bytes;
     ssize_t             recv_bytes;
     int                 recv_sock;
@@ -27,6 +27,36 @@ int    scan_port(t_thread_context *ctx, int port)
 
     // timeput de recepción
     struct timeval timeout = {3, 0};
+    if (setsockopt(ctx->conf->sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1)
+    {
+        printf("ft_nmap: setsockopt (SO_RCVTIMEO): %s\n", strerror(errno));
+        close(ctx->conf->sockfd);
+        close(recv_sock);
+        return (-1);
+    }
+    
+    memset(&dest, 0, sizeof(dest));
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(port);
+    dest.sin_addr = ctx->conf->ip_address;
+
+    struct timeval start;
+    struct timeval end;
+    gettimeofday(&start, NULL);
+
+    // enviar UDP
+    sent_bytes = sendto(ctx->conf->sockfd, "X", 1, 0, (struct sockaddr *)&dest, sizeof(dest));
+    if (sent_bytes < 0)
+        printf("ft_nmap: sendto ( %s )\n", strerror(errno));
+    
+    // recibir ICMP
+    addr_len_recv = sizeof(recv_adr);
+    recv_bytes = recvfrom(recv_sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&recv_adr, &addr_len_recv);
+    gettimeofday(&end, NULL);
+    if (recv_bytes < 0)
+        printf("ft_nmap: recvfrom ( %s )\n", strerror(errno));
+    if (analysis_flags(buffer, recv_bytes, ctx->conf->ip_address, dest_port) == -1)
+        printf("ft_nmap: scan flags ( %s )\n", buffer);        
 
     return (0);
 }
